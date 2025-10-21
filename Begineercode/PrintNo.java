62dbbaf7e583f915345fac78eede6e3be57733fe
Abhay Garodi <abhayhgarodi365@gmail.com>
2025-10-21 22:39:46 +0530

    Add files via upload

---
+++ Begineercode/PrintNo.java
@@ -0,0 +1,8 @@
+public class PrintNo{
+	public static void main(String[] args) {
+		System.out.println("Number Between 10-110 : ");
+		for(int i=10; i<=110; i++){
+			System.out.print(i+ " ");
+		}
+	}
+}
